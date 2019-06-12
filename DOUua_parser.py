691422124dda012dee6307dbc38df51e5ced4b27
marzique <marzique@gmail.com>
2019-06-13 01:00:11 +0300

    dictionary

--- DOUua_parser.py
+++ DOUua_parser.py
@@ -1,29 +1,29 @@
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen
 
-my_url = "https://jobs.dou.ua/vacancies/?category=Python&beginners="
 
-# connect to url
-Client = urlopen(my_url)
+def get_vacancies():
+    my_url = "https://jobs.dou.ua/vacancies/?category=Python&exp=0-1"
 
-# get raw html
-page_html = Client.read()
+    Client = urlopen(my_url)
+    page_html = Client.read()
+    Client.close()
 
-# disconnect
-Client.close()
+    page_soup = soup(page_html, "html.parser")
 
-# parse that dada as html
-page_soup = soup(page_html, "html.parser")
+    vacancies_list = page_soup.findAll("li", {"class": "l-vacancy"})
+    vacancies = []
+    for vacancy in vacancies_list:
+        name = vacancy.div.div.a.get_text()
+        city = vacancy.div.div.findAll("span", {"class": "cities"})[0].get_text()
+        company = vacancy.div.div.strong.get_text()
+        if city in ["Киев", "Київ", "Kyiv"]:
+            vacancy = {}
+            vacancy["name"] = name
+            vacancy["company"] = company
+            vacancies.append(vacancy)
 
-# list of vacancies at the page
-vacancies = page_soup.findAll("li", {"class": "l-vacancy"})
+    print(vacancies)
 
-for vacancy in vacancies:
-    sign = vacancy.div.div.a.string
-    city = vacancy.div.div.findAll("span", {"class": "cities"})[0].text
-    company = vacancy.div.div.strong.get_text()
-    print(sign)
-    print(company)
-    print(city)
-    print()
 
+get_vacancies()
